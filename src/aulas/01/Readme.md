e3a487a7c9d46c6e8f1588ee35839ab5c1c21c90
Luana Alves <luanaalvessoares_@outlook.com>
2024-05-20 09:51:19 -0300

    update: day 01

--- src/aulas/01/Readme.md
+++ src/aulas/01/Readme.md
@@ -8,4 +8,10 @@ Estudos sobre uso do método _echo_, declaração de uma variável, _short tags_
 
 3. _Short tags_ é uma maneira rápida e implicita de imprimir o valor de uma variável.
 
-4. Comentários no PHP podem ser feitos utilizando _//_ na frente da mensagem.
+4. Comentários no PHP podem ser feitos utilizando _//_, _#_ ou _/**/_ para comentários multi linhas.
+
+5. _phpinfo()_ é uma função utilizada para imprimir informações sobre o PHP e suas dependências, também exibe versões dos pacotes instalados e é útil para saber como o servidor está configurado.
+
+6. O PHP não é case sensitive para suas próprias instruções, somente para as variáveis então: _echo_, _EcHo_ ou _ECHO_ funcionam igualmente, mas _$name_, _$Name_ ou _$NAME_ são todas variáveis diferentes, uma não remete a outra.
+
+7. Assim como em outras linguagens de programação, instruções com mais de uma linha no PHP serão delimitadas com chaves (_{}_).
--- src/aulas/01/app.php
+++ src/aulas/01/app.php
@@ -7,24 +7,42 @@
 ?>
 
 
+<?php
+// Treinamento
+
+// Crie uma variável chamada $nome e atribua a ela o seu próprio nome como um valor de string. Em seguida, use o echo para imprimir uma saudação que inclua o nome armazenado na variável. Por exemplo, se o seu nome for "João", o programa deve imprimir "Olá, João!".
+    $name = "João";
+    echo "Olá, $name! Como vai? <br>";
+
+// Declare duas variáveis numéricas, $numero1 e $numero2, e atribua valores a elas. Use o echo para imprimir o resultado da soma dessas duas variáveis. Por exemplo, se $numero1 for 5 e $numero2 for 3, o programa deve imprimir "O resultado da soma é 8".
+    $value1 = 4;
+    $value2 = 9;
+    echo "O resultado da soma de $value1 e $value2 é " . ($value1 + $value2);
+
+// Crie duas variáveis de string, $primeiraParte e $segundaParte. Atribua a $primeiraParte o valor "PHP é " e a $segundaParte o valor "incrível!". Use o echo para imprimir a concatenação dessas duas strings, resultando na frase completa "PHP é incrível!".
+    $firstPart = "O PHP";
+    $lastPart = "é incrível!";
+
+    echo "<br> $firstPart $lastPart <br>";
+    echo $firstPart . $lastPart;
+?>
 
 
 <?php
-//Treinamento
+    phpinfo();
+?>
 
-//Crie uma variável chamada $nome e atribua a ela o seu próprio nome como um valor de string. Em seguida, use o echo para imprimir uma saudação que inclua o nome armazenado na variável. Por exemplo, se o seu nome for "João", o programa deve imprimir "Olá, João!".
-$name = "João";
-echo "Olá, $name! Como vai? <br>";
+<?php
+// Tipos de comentários:
 
-//Declare duas variáveis numéricas, $numero1 e $numero2, e atribua valores a elas. Use o echo para imprimir o resultado da soma dessas duas variáveis. Por exemplo, se $numero1 for 5 e $numero2 for 3, o programa deve imprimir "O resultado da soma é 8".
-$value1 = 4;
-$value2 = 9;
-echo "O resultado da soma de $value1 e $value2 é " . ($value1 + $value2);
+// Este é um comentário
+# Este também é um comentário
+/*
+    Este
+    também
+    é
+    um
+    comentário
+*/
 
-//Crie duas variáveis de string, $primeiraParte e $segundaParte. Atribua a $primeiraParte o valor "PHP é " e a $segundaParte o valor "incrível!". Use o echo para imprimir a concatenação dessas duas strings, resultando na frase completa "PHP é incrível!".
-$firstPart = "O PHP";
-$lastPart = "é incrível!";
-
-echo "<br> $firstPart $lastPart <br>";
-echo $firstPart . $lastPart;
 ?>
