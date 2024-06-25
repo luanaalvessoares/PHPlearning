5a77676e455ba16e3f32fe011e82c6fcd34380a4
Luana Alves <luanaalvessoares_@outlook.com>
2024-06-25 06:46:32 -0300

    feat: escopo global

--- src/aulas/03/Readme.md
+++ src/aulas/03/Readme.md
@@ -5,3 +5,5 @@
 01. _Escopo local_
 O escopo local é basicamente o corpo de uma função. Uma variável local deve respeitar o espaço entre a função onde está localizada sem afetar algo no escopo global do código. Ela não é acessível fora desse escopo e sempre retornam ao seu valor inicial, ou seja, uma função após ser rodada pode ter seus valores modificados mas após esse evento tudo dentro da função retorna ao seu valor original.
 
+02. _Escopo global_
+Escopo global é basicamente tudo que está ao redor de escopos protegidos por chaves ({}), ou seja, tudo que está ao redor de maneira "livre". Uma variável de escopo global é sempre declarada fora de uma função e não é acessível, por padrão, dentro de uma função a fim de prevenir problemas de software. Para acessar uma variável global dentro de um escopo local precisaremos utilizar a palavra reservada _global_.
--- src/aulas/03/app.php
+++ src/aulas/03/app.php
@@ -14,3 +14,37 @@
 
     echo "<br><br> ------------------------------------------- <br><br>";
 ?>
+
+
+<?php
+// Testes de uma variável de escopo global
+    $name = "Marcos";
+
+    function printName() {
+        echo "<br>O nome do cliente é $name<br>";
+    }
+
+    printName(); // A impressão é somente 'O nome do cliente é' pois não consegue acessar a variável global
+
+    function printName2() {
+        global $name; // Utilizando a palavra-chave 'global' para acessar a variável
+
+        echo "<br>O nome do cliente é $name<br>";
+    }
+
+    printName2(); // A impressão é 'O nome do cliente é Marcos'
+
+
+    function printName3() {
+        global $name;
+
+        $name = "Paulo"; // Mudando o valor da variável no escopo local utilizado a palavra-chave 'global'
+
+        echo "<br>O nome do cliente é $name<br>";
+    }
+
+    printName3();
+
+    echo "<br>O novo nome do cliente agora é $name<br>";
+
+?>
